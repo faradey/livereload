de9f8c8d5939e445e4908bc54b3c4950d3ba532d
faradey <rvansp@gmail.com>
2022-09-27 23:23:47 +0300

    click on extension icon v3

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ let tabRelStatuses = {};
 chrome.action.onClicked.addListener((tab) => {
     if (tabRelStatuses[tab.id] !== true) {
         tabRelStatuses[tab.id] = true;
+        connectToSocket(tab);
     } else {
         tabRelStatuses[tab.id] = false;
         selectTab(tab.id);
@@ -21,4 +22,37 @@ function selectTab(tabId) {
     } else {
         chrome.action.setIcon({'path': {128: 'images/icon_livereload_red128.png'}});
     }
+}
+
+function connectToSocket(tab){
+    let host = (new URL(tab.url));
+    let socket = new WebSocket("ws://"+host.hostname+":35729/livereload");
+    socket.onopen = function() {
+        tabRelStatuses[tab.id] = true;
+        selectTab(tab.id);
+        console.log("Connection established.");
+    };
+    socket.onclose = function(event) {
+        tabRelStatuses[tab.id] = false;
+        selectTab(tab.id);
+        if (event.wasClean) {
+            console.log('Connection closed cleanly');
+        } else {
+            console.log('Lost connection');
+        }
+        console.log('Code: ' + event.code + ' reason: ' + event.reason);
+    };
+
+    socket.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        if(data['command'] === "reload") {
+            console.log(data['path']);
+        }
+    };
+
+    socket.onerror = function(error) {
+        tabRelStatuses[tab.id] = false;
+        selectTab(tab.id);
+        console.log("Error: " + error.message);
+    };
 }
