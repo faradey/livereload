ec5212a22cf5a73e54d5b64bb432155d979e0424
faradey <rvansp@gmail.com>
2022-09-27 22:40:24 +0300

    click on extension icon v2

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+let tabRelStatuses = {};
 chrome.action.onClicked.addListener((tab) => {
-    chrome.action.setIcon({'path': {128: 'images/icon_livereload_green128.png'}});
+    if (tabRelStatuses[tab.id] !== true) {
+        tabRelStatuses[tab.id] = true;
+    } else {
+        tabRelStatuses[tab.id] = false;
+        selectTab(tab.id);
+    }
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    selectTab(tabId);
+});
+chrome.tabs.onActivated.addListener((info) => {
+    selectTab(info.tabId);
+});
+
+function selectTab(tabId) {
+    if (tabRelStatuses[tabId] === true) {
+        chrome.action.setIcon({'path': {128: 'images/icon_livereload_green128.png'}});
+    } else {
+        chrome.action.setIcon({'path': {128: 'images/icon_livereload_red128.png'}});
+    }
+}
