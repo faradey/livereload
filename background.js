afe1d07da1908f3ee057ce59294ec9b8168faa75
faradey <rvansp@gmail.com>
2022-09-28 23:25:41 +0300

    reload css files

--- background.js
+++ background.js
@@ -1,13 +1,50 @@
 let tabRelStatuses = {};
+let connections = {};
 chrome.action.onClicked.addListener((tab) => {
     if (tabRelStatuses[tab.id] !== true) {
         tabRelStatuses[tab.id] = true;
         connectToSocket(tab);
     } else {
         tabRelStatuses[tab.id] = false;
-        selectTab(tab.id);
+        connections[tab.id].close();
     }
+
+    selectTab(tab.id);
 });
+
+function connectToSocket(tab) {
+    let url = (new URL(tab.url));
+    connections[tab.id] = new WebSocket("ws://" + url.host + ":35729/livereload");
+    connections[tab.id].onopen = function () {
+        tabRelStatuses[tab.id] = true;
+        selectTab(tab.id);
+        console.log("Connection established.");
+    };
+    connections[tab.id].onclose = function (event) {
+        tabRelStatuses[tab.id] = false;
+        selectTab(tab.id);
+        if (event.wasClean) {
+            console.log('Connection closed cleanly');
+        } else {
+            console.log('Lost connection');
+        }
+        console.log('Code: ' + event.code + ' reason: ' + event.reason);
+    };
+
+    connections[tab.id].onmessage = function (event) {
+        let data = JSON.parse(event.data);
+        console.log(data);
+        if (data['command'] === "reload") {
+            reload(tab.id, data['path']);
+        }
+    };
+
+    connections[tab.id].onerror = function (error) {
+        tabRelStatuses[tab.id] = false;
+        selectTab(tab.id);
+        console.log("Error: " + error.message);
+    };
+}
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     selectTab(tabId);
@@ -24,35 +61,8 @@ function selectTab(tabId) {
     }
 }
 
-function connectToSocket(tab){
-    let host = (new URL(tab.url));
-    let socket = new WebSocket("ws://"+host.hostname+":35729/livereload");
-    socket.onopen = function() {
-        tabRelStatuses[tab.id] = true;
-        selectTab(tab.id);
-        console.log("Connection established.");
-    };
-    socket.onclose = function(event) {
-        tabRelStatuses[tab.id] = false;
-        selectTab(tab.id);
-        if (event.wasClean) {
-            console.log('Connection closed cleanly');
-        } else {
-            console.log('Lost connection');
-        }
-        console.log('Code: ' + event.code + ' reason: ' + event.reason);
-    };
-
-    socket.onmessage = function(event) {
-        let data = JSON.parse(event.data);
-        if(data['command'] === "reload") {
-            console.log(data['path']);
-        }
-    };
-
-    socket.onerror = function(error) {
-        tabRelStatuses[tab.id] = false;
-        selectTab(tab.id);
-        console.log("Error: " + error.message);
-    };
+function reload(tabId, path) {
+    chrome.tabs.sendMessage(tabId, {actionType: "reload", path: path}, function (response) {
+        console.log(response);
+    });
 }
---
+++ madock-livereload.js
@@ -0,0 +1,28 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        if (request.actionType === "reload") {
+            let paths = request.path.split("/");
+            paths.reverse();
+            let transformer = "";
+            let isMatch = false;
+            paths.forEach((val, i, arr) => {
+                let elms = document.querySelectorAll("link[href*='" + val + "']");
+                if (isMatch === false) {
+                    if (elms.length !== 1) {
+                        if (transformer) {
+                            transformer = val + "/" + transformer;
+                        } else {
+                            transformer = val;
+                        }
+                    } else {
+                        isMatch = true;
+                        let queryString = '?reload=' + new Date().getTime();
+                        elms[0].href = elms[0].href.replace(/\?.*|$/, queryString);
+                    }
+                }
+            });
+        }
+        sendResponse({actionType: "reload", result: "ok"});
+    }
+);
